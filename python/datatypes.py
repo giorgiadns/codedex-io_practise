22d3ac034af9754888d82c30d669501301d205df
Giorgia De Nardis <128826431+giorgiadns@users.noreply.github.com>
2025-05-27 14:34:29 +0100

    Create datatypes.py
    Introduction to the main data types in python.

---
+++ python/datatypes.py
@@ -0,0 +1,33 @@
+# DATA TYPES
+#-----------------------------------------------
+# Integer
+
+# An integer (int), is a whole number. It has no decimal
+# point and contains the number 0, positive and negative counting
+# numbers.
+
+my_age = 23 
+
+#-----------------------------------------------
+# Float
+
+# A floating- point number (float) is a decimal number.
+
+my_height = 1.70 
+
+#-----------------------------------------------
+# String
+
+# A string (str), is used to store text.
+
+my_name = "Giorgia"
+my_username = "giorgiadns"
+
+#-----------------------------------------------
+# Boolean
+
+# A boolean data type (bool), stores a value that can
+# only be either true or false. Remember, in Python it's
+# capitalized 'True' or 'False'!
+
+learning_python = True
