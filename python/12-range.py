717eacf775282652e317674b4805c3f16f105cad
Giorgia De Nardis <128826431+giorgiadns@users.noreply.github.com>
2025-05-27 20:48:01 +0100

    Create 12-range.py

---
+++ python/12-range.py
@@ -0,0 +1,20 @@
+# RANGE
+
+# To loop through a set of code a specified number 
+# of times, we can use a for loop and the range() 
+# function.
+
+# The range() function returns a sequence of numbers.
+# By default, the sequence starts at 0 and increment
+# by 1, ending at a specified number.
+#-----------------------------------------------
+
+#PRACTISE
+
+# Suppose you got detention and the teacher wants 
+# you to write "I will not use Snapchat in class" 
+# on the whiteboard 100 times.
+#-----------------------------------------------
+
+for i in range(100):
+  print("I will not use Snapchat in class.")
